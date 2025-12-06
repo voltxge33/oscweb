2a546610dc53f383a54dded2a6d47db196a6250f
Oliver <theleonardfamilycalendar@gmail.com>
2025-12-06 15:23:46 -0500

    idfk

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const OSC = require('osc-js')
-const config = { udpClient: { port: 5198 } }
+const config = { udpClient: { port: 5198, host: '10.0.0.33' } }
 const osc = new OSC({ plugin: new OSC.BridgePlugin(config) })
 osc.open() // start a WebSocket server on port 8080
